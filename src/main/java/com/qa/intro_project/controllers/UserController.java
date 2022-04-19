32a63f3ddc605d8df6e90bf624bacf837b9bf7e7
MrWalshyType2 <mwalsh@qa.com>
2022-04-19 19:54:45 +0100

    Added validation example and tasks

--- src/main/java/com/qa/intro_project/controllers/UserController.java
+++ src/main/java/com/qa/intro_project/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.qa.intro_project.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,28 +34,18 @@ public class UserController {
 	// - specify a variable in a path by surrounding it in curly braces
 	@GetMapping(path = "/{id}") // localhost:8080/user/3
 	public User getUser(@PathVariable(name = "id") int id) {
-		// we need to get the id out of the path, this can be done with the @PathVariable annotation
-		// - the 'name' attribute takes the name of the path variable
-		// - this then gets the path variable from the path and inserts it into the parameter 'int id'
 		for (int i = 0; i < users.size(); i++) {
 			if (this.users.get(i).getId() == id) {
 				return this.users.get(i);
 			}
 		}
 		return null; // we should return a 404 not found response code
-		
-//		User user = users.stream()
-//						  .filter(u -> u.getId() == id)
-//						  .findFirst()
-//						  .orElse(null);
-//		return user;
 	}
 	
 	// POST
 	@PostMapping // indicates POST requests are accepted at localhost:8080/user
-	public User createUser(@RequestBody User user) {
-		// @RequestBody tells Spring to retrieve the data in the requests body and then create an instance
-		// of User (using the default empty constructor) and then calls its mutators (setters) to set its values
+	public User createUser(@Valid @RequestBody User user) {
+		// The call to 
 		user.setId(COUNTER++);
 		users.add(user);
 		return user;
@@ -62,14 +54,14 @@ public class UserController {
 	// PUT
 	@PutMapping(path = "/{id}")
 	public User updateUser(@RequestBody User user, @PathVariable(name = "id") int id) {
-		// TODO: Implement me
+		// TODO: In your implementation, ensure @Valid is called on the request body before updating
 		return null;
 	}
 	
 	// DELETE
 	@DeleteMapping(path = "/{id}")
 	public User deleteUser(@PathVariable(name = "id") int id) {
-		// TODO: Implement me
+		// Your implementation here
 		return null;
 	}
 }
--- src/main/java/com/qa/intro_project/data/entity/User.java
+++ src/main/java/com/qa/intro_project/data/entity/User.java
@@ -1,9 +1,37 @@
 package com.qa.intro_project.data.entity;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+// Spring has a 'spring-boot-starter-validation' module which transitively includes
+// the 'hibernate-validator' dependency. This can be used to apply validation annotations
+// to data transfer or domain objects.
+// 
+// The preferred method is to annotate your fields with validation attributes, but you
+// can annotate methods. Do not do both to avoid unnecessarily validating twice.
+//
+// Validation must be triggered via the @Valid annotation, see `UserController -> createUser()`.
+//
+// Validation annotations include:
+// - @Email            Applies strict email validation according to the Bean validation provider
+// - @NotBlank         The field must not be null and contain at least 1 non-whitespace character
+// - @Min @Max         Used to specify the minimum and maximum ranges of a numeric value respectively (inclusive)
+// - @Past @Future     Used to specify that a date should be in the past or the future respectively
+// - @PastOrPresent    Specifies that the date should be in the past or now
+// - @FutureOrPresent  Specifies that the date should be in the future or now
+// - @Pattern          Use to provide a regex pattern
+
 public class User {
 
 	private int id;
+	
+	@NotNull
+	@NotBlank
+	@Size(min = 2, max = 16, message = "Username must have at least 2 characters, but no more than 16")
 	private String username;
+	
+	// TODO: Add 3 new fields to the User class, with appropriate validation annotations applied to each
 	
 	public User() {
 		
