3eb2fef78f5ef620884f883be6886e2c8638a029
MrWalshyType2 <mwalsh@qa.com>
2022-04-19 19:36:03 +0100

    Initialised repo with initial work done

---
+++ src/test/java/com/qa/intro_project/IntroProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.intro_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
