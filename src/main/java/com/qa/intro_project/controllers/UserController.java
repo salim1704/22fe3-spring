23bdf1ff4ac39f69ef598b03ff78ca3682c805f7
abdul salim <qhs9876@gmail.com>
2022-04-20 10:08:54 +0100

    user controller updated

--- src/main/java/com/qa/intro_project/controllers/UserController.java
+++ src/main/java/com/qa/intro_project/controllers/UserController.java
@@ -21,55 +21,79 @@ public class UserController {
 	// the list of users is just pretending to be a database for now
 	private static int COUNTER = 1;
 	private List<User> users = new ArrayList<>(List.of(new User(COUNTER++, "Fred"), new User(COUNTER++, "Sarah")));
-	
+
 	// GET
 	@GetMapping
 	public List<User> getUsers() {
 		return users;
 	}
-	
+
 	// GET by id
 	// - specify a variable in a path by surrounding it in curly braces
 	@GetMapping(path = "/{id}") // localhost:8080/user/3
 	public User getUser(@PathVariable(name = "id") int id) {
-		// we need to get the id out of the path, this can be done with the @PathVariable annotation
+		// we need to get the id out of the path, this can be done with the
+		// @PathVariable annotation
 		// - the 'name' attribute takes the name of the path variable
-		// - this then gets the path variable from the path and inserts it into the parameter 'int id'
+		// - this then gets the path variable from the path and inserts it into the
+		// parameter 'int id'
 		for (int i = 0; i < users.size(); i++) {
 			if (this.users.get(i).getId() == id) {
 				return this.users.get(i);
 			}
 		}
 		return null; // we should return a 404 not found response code
-		
+
 //		User user = users.stream()
 //						  .filter(u -> u.getId() == id)
 //						  .findFirst()
 //						  .orElse(null);
 //		return user;
 	}
-	
+
 	// POST
 	@PostMapping // indicates POST requests are accepted at localhost:8080/user
 	public User createUser(@RequestBody User user) {
-		// @RequestBody tells Spring to retrieve the data in the requests body and then create an instance
-		// of User (using the default empty constructor) and then calls its mutators (setters) to set its values
+		// @RequestBody tells Spring to retrieve the data in the requests body and then
+		// create an instance
+		// of User (using the default empty constructor) and then calls its mutators
+		// (setters) to set its values
 		user.setId(COUNTER++);
 		users.add(user);
 		return user;
 	}
-	
+
 	// PUT
 	@PutMapping(path = "/{id}")
 	public User updateUser(@RequestBody User user, @PathVariable(name = "id") int id) {
-		// TODO: Implement me
-		return null;
+		// Get the user from the list
+		User savedUser = null;
+		for (int i = 0; i < users.size(); i++) {
+			if (this.users.get(i).getId() == id) {
+				savedUser = this.users.get(i);
+			}
+		}
+		// Update that user
+		if (savedUser != null) {
+			savedUser.setUsername(user.getUsername());
+		}
+		// Return that user
+		return savedUser;
 	}
-	
+
 	// DELETE
 	@DeleteMapping(path = "/{id}")
 	public User deleteUser(@PathVariable(name = "id") int id) {
-		// TODO: Implement me
-		return null;
+		// Get the user
+		User user = null;
+		for (int i = 0; i < users.size(); i++) {
+			if (this.users.get(i).getId() == id) {
+				user = this.users.get(i);
+			}
+		}
+		// Remove the user
+		users.remove(user);
+		// Return the removed user
+		return user;
 	}
 }
